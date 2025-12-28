b7c567982cfd3e6a15f5df0fd32a08371f272e9a
amleshtikar52 <amleshkewat52@gmail.com>
2025-12-28 20:11:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,8 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-BOT_OWNER_ID = int(os.getenv("BOT_OWNER_ID"))
+# ⚠️ YAHI BOT CREATOR OWNER ID HAI (sirf 1)
+BOT_OWNER_ID = int(os.getenv("BOT_OWNER_ID", "123456789"))
+
+GRID_NORMAL = 8
+GRID_HARD = 14
